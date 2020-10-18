40e111aff80505935c1f5249134be2f2aecf849f
Mark <wowmarkambro@gmail.com>
2020-10-17 23:04:02 -0500

    💄 focusing on Tailwind, fixed homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,16 @@
 module.exports = {
-  future: {
-    // removeDeprecatedGapUtilities: true,
-    // purgeLayersByDefault: true,
-  },
-  purge: {
-    enabled: true,
-    layers: ['base', 'components', 'utilities'],
-    content: ['./**/*.html', './**/*.pug', './**/*.js'],
-  },
-  theme: {
-    extend: {},
-  },
-  variants: {},
-  plugins: [],
+	future: {
+		// removeDeprecatedGapUtilities: true,
+		// purgeLayersByDefault: true,
+	},
+	purge: {
+		enabled: false,
+		layers: ["base", "components", "utilities"],
+		content: ["./**/*.html", "./**/*.pug", "./**/*.js"],
+	},
+	theme: {
+		extend: {},
+	},
+	variants: {},
+	plugins: [],
 };
--- webpack.config.js
+++ webpack.config.js
@@ -4,53 +4,54 @@
  * CC BY-NC-SA 4.0
  */
 
-const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 // handle js files
 const javascript = {
-  test: /\.(js)$/,
-  use: [
-    {
-      loader: 'babel-loader',
-      options: { presets: ['env'] },
-    },
-  ],
+	test: /\.(js)$/,
+	use: [
+		{
+			loader: "babel-loader",
+			options: { presets: ["env"] },
+		},
+	],
 };
 
 // handle sass/css imports
 const styles = {
-  test: /\.s[ac]ss$/i,
-  use: [
-    {
-      loader: MiniCssExtractPlugin.loader,
-    },
-    'css-loader',
-    'postcss-loader',
-    'sass-loader',
-  ],
+	test: /\.s[ac]ss$/i,
+	use: [
+		{
+			loader: MiniCssExtractPlugin.loader,
+		},
+		"css-loader",
+		"postcss-loader",
+		"sass-loader",
+	],
 };
 
 const config = {
-  entry: {
-    app: './public/javascript/funding-app.js',
-  },
-  // devtool: 'source-map',
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'public', 'dist'),
-  },
-  module: {
-    rules: [javascript, styles],
-  },
-  optimization: {
-    minimize: true,
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: 'style.css',
-    }),
-  ],
+	mode: "development",
+	entry: {
+		app: "./public/javascript/funding-app.js",
+	},
+	// devtool: 'source-map',
+	output: {
+		filename: "[name].bundle.js",
+		path: path.resolve(__dirname, "public", "dist"),
+	},
+	module: {
+		rules: [javascript, styles],
+	},
+	optimization: {
+		minimize: true,
+	},
+	plugins: [
+		new MiniCssExtractPlugin({
+			filename: "style.css",
+		}),
+	],
 };
 // fix some webpack issues
 process.noDeprecation = true;
