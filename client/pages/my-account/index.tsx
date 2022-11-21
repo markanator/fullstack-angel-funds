93eb326e631b895d7f83b3e590de6f46cd779b68
Mark Ambrocio <wowmarkambro@gmail.com>
2022-11-20 20:53:38 -0700

    client: update dashboard

--- client/pages/my-account/index.tsx
+++ client/pages/my-account/index.tsx
@@ -1,4 +1,4 @@
-import { Container, Flex, Text } from "@chakra-ui/react";
+import { Container, Flex, Text, Image } from "@chakra-ui/react";
 import { NextPage } from "next";
 import React from "react";
 import { useIsAuth } from "utils/useIsAuth";
@@ -16,7 +16,21 @@ export default function MyAccountPage({}: IAccountProps) {
       <AuthBanner bgImage="/images/breadcrumb.png" title="My Account" />
       <Flex bgColor="gray.200" w="full" h="full">
         <Container maxW="7xl" py="2rem">
-          {!isLoggedIn ? <Text>Loading...</Text> : <AccountNavbar />}
+          {!isLoggedIn ? (
+            <Text>Loading...</Text>
+          ) : (
+            <>
+              <AccountNavbar />
+              <Image
+                src="/images/newFeatureComingSoon1.png"
+                alt="new feature coming soon"
+                width="100%"
+                height="auto"
+                mt={8}
+                shadow="lg"
+              />
+            </>
+          )}
         </Container>
       </Flex>
     </Layout>
