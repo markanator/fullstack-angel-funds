bfd1e58a0055a9735b2b164fb294e947094ed698
Mark Ambrocio <wowmarkambro@gmail.com>
2022-11-21 22:41:47 -0700

    server: fix migrations

--- backend/prisma/migrations/20221122054109_add_project_rewards/migration.sql
+++ backend/prisma/migrations/20221122054109_add_project_rewards/migration.sql
@@ -2,10 +2,11 @@
 CREATE TABLE "Reward" (
     "id" SERIAL NOT NULL,
     "amount" INTEGER NOT NULL,
+    "title" TEXT NOT NULL,
     "image" TEXT,
     "description" TEXT NOT NULL,
-    "deliveredByMonth" TIMESTAMP(3) NOT NULL,
-    "deliveredByYear" TIMESTAMP(3) NOT NULL,
+    "deliveredByMonth" TEXT NOT NULL,
+    "deliveredByYear" TEXT NOT NULL,
     "quantityRemaining" INTEGER NOT NULL,
     "projectId" INTEGER NOT NULL,
 
